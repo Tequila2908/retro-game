7f24d004975bf64ee098f636f551ffbd92550c86
macbook air <macbookair@MacBook-Air-macbook.local>
2020-06-25 15:01:11 +0300

    fix

--- src/js/__tests__/allowedPositions.test.js
+++ src/js/__tests__/allowedPositions.test.js
@@ -1,6 +1,5 @@
 import allowedPositions from '../allowedPositions';
 
-
 test('allowed positions test', () => {
   const received = allowedPositions(2, 2, 8);
   const expected = [3, 1, 10, 11, 9, 4, 0, 18, 20, 16];
